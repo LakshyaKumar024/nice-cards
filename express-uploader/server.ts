25adfe4e195c7ba4e8b0a9f0cf69f2e081e7eb8f
Lakshyakumar266 <lakshyakumar0098@gmail.com>
2025-11-23 00:56:41 +0530

    prod fix 2

--- express-uploader/server.ts
+++ express-uploader/server.ts
@@ -34,7 +34,8 @@ app.use(cors({
 
 
 
-app.use(express.json());
+app.use(express.json({ limit: "500mb" }));
+app.use(express.urlencoded({ limit: "500mb", extended: true }));
 
 // ------------------------
 // 📌 Resolve root folder
